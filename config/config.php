19db5baa77ff3314a852b70af53752aa69fc2b2d
Toshiki Sugiyama <s.toshiki.llwz8869@gmail.com>
2018-11-13 17:41:35 +0900

    ログイン画面に飛ばす

--- config/config.php
+++ config/config.php
@@ -6,6 +6,8 @@ define('DSN', 'mysql:host=localhost;dbname=sns_php');
 define('DB_USERNAME', 'dbuser');
 define('DB_PASSWORD', 'aij71975');
 
+define('SITE_URL', 'http://'.$_SERVER['HTTP_HOST']);
+
 require_once(__DIR__.'/../lib/functions.php');
 require_once(__DIR__.'/autoload.php');
 
---
+++ lib/Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespase MyApp;
+
+class Controller {
+  protected function isLoggedIn(){
+    // $_SESSION['me']
+    return isset($_SESSION['me']) && !empty($_SESSION['me']);
+  }
+}
