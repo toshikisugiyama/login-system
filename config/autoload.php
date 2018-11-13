085d5a0567030d73122b74a56a5bf29205cf9d62
Toshiki Sugiyama <s.toshiki.llwz8869@gmail.com>
2018-11-13 16:41:36 +0900

    autoload の設定

--- config/autoload.php
+++ config/autoload.php
@@ -6,3 +6,14 @@ index.php controller
 MyApp\Controller\Index
 -> lib/Controller/Index.php
 */
+
+spl_autoload_register(function($class){
+  $prefix = 'MyApp\\';
+  if(strpos($class, $prefix)===0){
+    $className = substr($class, strlen($prefix));
+    $classFilePath = __DIR__.'/../lib/'.str_replace('\\', '/', $className).'.php';
+    if (file_exists($classFilePath)) {
+      require $classFilePath;
+    }
+  }
+});
